cc53024b2a8395de6624dd43eac2dc1033d14463
Ethan Crann <ecrann@protonmail.com>
2020-05-27 11:55:48 -0300

    Initial commit

---
+++ Blackjack.py
@@ -0,0 +1,79 @@
+from Deck import Deck
+
+class Blackjack(self):
+
+    def __init__(self, initial_balance)
+        self.player_hand = []
+        self.dealer_hand = []
+        
+        self.player_tot = 0
+        self.dealer_tot = 0
+
+        self.winner = False
+
+        self.deck = Deck()
+        self.deck.init_deck()
+
+        self.player_balance = initial_balance
+
+    def player_hit(self):
+        self.player_hand.append(self.deck.draw())
+        calc_total("Player")
+
+    def player_stand(self):
+        self.check_winner()
+
+    def dealer_hit(self):
+        self.dealer_hand.append(self.deck.draw())
+        calc_total("Dealer")
+
+    def calc_total(self, who):
+        if who == "Player":
+            hand = self.player_hand
+            total = self.player_tot
+        else:
+            hand = self.dealer_hand
+            total = self.dealer_tot
+        aces = 0
+        for card in hand:
+            if card.value in "JQK":
+                total += 10
+            elif card.value in "2345678910":
+                total += int(card.value)
+            else:
+                aces += 1
+        if aces:
+            total += aces
+            while aces > 0:
+                if total < 21:
+                    total+10
+                aces -= 1
+        if total
+
+    def dealers_turn(self):
+        while self.dealer_tot < 17:
+            self.dealer_hit()
+        check_winner
+    
+    def check_winner(self):
+        if self.player_tot > 21:
+            self.winner = "Dealer"    
+        elif self.player_tot == 21:
+            if self.dealer_tot == 21:
+                self.winner = "Draw"
+            elif self.player_tot > self.dealer_tot:
+                self.winner = "Player"
+        else self.player_tot < 21:
+            if self.dealer_tot > self.player_tot:
+                self.winner = "Dealer"
+            elif self.dealer_tot == self.player_tot:
+                self.winner = "Draw"
+            else:
+                self.winner = "Player"
+
+    def new_game(self):
+        
+
+if __name__ == "__main__":
+    deck = Deck()
+    print(deck.cards)
---
+++ Deck.py
@@ -0,0 +1,37 @@
+import random as random
+
+class Deck:
+
+    def __init__(self):
+        self.cards = []
+        self.discard = []
+
+    def discard(self,card):
+        self.discard.append(card)
+    
+    def draw(self):
+        card = self.cards.pop()
+        return card
+
+    def init(self):
+        suits = ["Spades","Hearts","Clubs","Diamonds"]
+        values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+        for suit in suits:
+            for value in values:
+                card = Card(suit,value)
+                self.cards.append(card)
+        self.shuffle_deck()
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def reset(self):
+        self.card = self.discard + self.cards
+        self.shuffle_deck()
+
+class Card:
+
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
