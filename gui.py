91d17eaada275f1eebcdd28143a49b5d2ea5964c
The Cryptic Canadian <ecrann@protonmail.com>
2020-05-29 13:05:26 -0230

    Began work on Gui

---
+++ gui.py
@@ -0,0 +1,34 @@
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton, QVBoxLayout 
+from PyQt5.QtGui import QIcon, QImage
+import sys
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        top = 400
+        left = 400
+        width = 800
+        height = 600
+        
+        self.hit_button = QPushButton("Hit")
+        self.stand_button = QPushButton("Stand")
+        self.ng_button = QPushButton("New Game")
+        
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hit_button)
+        self.layout.addWidget(self.stand_button)
+        self.layout.addWidget(self.ng_button)
+        self.setLayout(self.layout)
+        #icon
+
+        self.setWindowTitle("Blackjack")
+        self.setGeometry(top, left, width, height)
+        #self.setWindowIcon(QIcon(icon))
+        
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = Window()
+    window.show()
+    app.exec()
